dc8d0d2b14d52581159b79478cd8e17bdd081fd7
João Felipe Zini <joaofelipezini@gmail.com>
2023-08-07 18:17:46 -0300

    viewchild in angular8+

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,22 +16,27 @@ import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angu
 })
 export class ShoppingEditComponent {
   //the prop is passed between [] and accessed in the child component with @Input('alias')
-  @Input() ingredient: { name: string, amount: number }; 
-  @Output() addIngredientEvent = new EventEmitter<{ name: string, amount: number }>();
+  @Input() ingredient: { name: string; amount: number };
+  @Output() addIngredientEvent = new EventEmitter<{
+    name: string;
+    amount: number;
+  }>();
 
   // newIngredientName: string = ''; this is need in case of two-way binding, but I am using local ref instead
-  newIngredientAmount: number = 0;
-
+  // newIngredientAmount: number = 0;
+  @ViewChild('newIngredientAmount', { static: true }) // another way to access the value of the input field
+  newIngredientAmount: ElementRef;
 
   addIngredient(target: HTMLInputElement) {
-    this.addIngredientEvent.emit({ name: target.value, amount: this.newIngredientAmount });
+    this.addIngredientEvent.emit({
+      name: target.value,
+      amount: Number(this.newIngredientAmount.nativeElement.value), // this is the syntax of ViewChild
+    });
     // this.newIngredientName = '';
-    this.newIngredientAmount = 0;
+    // this.newIngredientAmount = 0;
   }
-  
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
